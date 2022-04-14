a5884d4ab29fd0c96f23d1c1c3e1288fbc672fe9
Jay Code <jayfullcode@gmail.com>
2022-04-14 21:24:53 +0200

    Add: error label + start js

---
+++ index.js
@@ -0,0 +1,8 @@
+const inputs = document.getElementsByTagName('input');
+const error_msg = document.getElementsByClassName("inscription-block__error");
+
+for(let i = 0 ; i < error_msg.length ; i++){
+    error_msg[i].style.display = "none";
+    // inputs[i].style.margin = "0 0 0 0";
+}
+
